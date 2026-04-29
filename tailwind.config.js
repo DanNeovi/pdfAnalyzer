46ddf2264c0576be0954b982b5357b55a6683b66
DanNeovi <danielb@neovi.co>
2026-04-29 09:30:52 -0700

    Refactorization of codebase
    splitting god file into multiple subsection pieces

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pdf-annotator.html",
+    "./src/**/*.{ts,tsx,js,jsx,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
